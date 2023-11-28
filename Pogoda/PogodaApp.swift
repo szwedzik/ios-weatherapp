c254d89f9806a93d09e7ae81d090c2e5bcad332b
asd <asd>
2023-11-28 19:33:39 +0100

    Initial Commit

---
+++ Pogoda/PogodaApp.swift
@@ -0,0 +1,17 @@
+//
+//  PogodaApp.swift
+//  Pogoda
+//
+//  Created by Mateusz Knop on 28/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct PogodaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
